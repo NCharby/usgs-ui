2be988bbc120162f235719d5034c26271443bcc7
Nick Charbonneau <nick.charby@gmail.com>
2021-02-26 19:09:08 -0800

    Last try

--- src/api/quake.api.js
+++ src/api/quake.api.js
@@ -9,7 +9,7 @@ import {
 export async function fetchQuakes (query: $UIRequestSearch): Promise<any> {
     //make heroku work
     const url = process.env.NODE_ENV === 'production'? 
-        'https://simpo-interview.herokuapp.com/api/0.1/': 
+        'https://simpo-interview.herokuapp.com/api/0.1/quakes': 
         '/api/0.1/quakes'
     try {
         const { data }: { data: $QuakeResponse } = await axios.get(url, {
